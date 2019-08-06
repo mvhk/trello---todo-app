9f9a8ae5eb0fd154056240346a739b815cad2aad
Venkata Harish Kumar Mamidi <harish.mamidi.kumar@gmail.com>
2019-08-06 10:03:37 +0530

    Remove all the Items added

--- todo.js
+++ todo.js
@@ -1,18 +1,35 @@
 var ul = document.getElementById("list");
 var li;
+
 var addButton = document.getElementById("add");
 addButton.addEventListener("click", addItem);
+
 var removeButton = document.getElementById("remove");
 removeButton.addEventListener("click", removeItems);
 
+var clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clearItems);
+
+// add the todo typed in the input
 function addItem() {
   console.log("add button clicked");
 }
+
+// to remove checked todos
 function removeItems() {
   li = ul.children;
-  console.log(li);
   for (let index = 0; index < li.length; index++) {
     if (li[index].children[0].checked) {
+      ul.removeChild(li[index]);
+    }
+  }
+}
+
+// to clear all elements
+function clearItems() {
+  li = ul.children;
+  for (let index = 0; index < li.length; index++) {
+    while (li[index]) {
       ul.removeChild(li[index]);
     }
   }
